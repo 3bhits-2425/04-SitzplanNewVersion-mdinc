3a529f11c9491478bc8a7650323d173a145dc957
mdinchtl <medine.dinc07@htl-salzburg.ac.at>
2024-12-05 10:23:30 +0100

    Chair position adjustments

--- Assets/MyGame/Scripts/Manager.cs
+++ Assets/MyGame/Scripts/Manager.cs
@@ -1,18 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class Manager : MonoBehaviour
-{    // Start is called before the first frame update
-    [SerializeField] private GameObject prefab;
-    [SerializeField] private GameObject myParent;
+// public class Manager : MonoBehaviour
+// {    // Start is called before the first frame update
+//     [SerializeField] private GameObject prefab;
+//     [SerializeField] private GameObject myParent;
 
-        void Start()
-        {
-            for (int i = 1; i < 100; i++)
-            {
-               GameObject obj = Instantiate(prefab);
-               obj.transform.SetParent(myParent.transform);
-            }
-        }
-    }
+//         void Start()
+//         {
+//             for (int i = 1; i < 100; i++)
+//             {
+//                GameObject obj = Instantiate(prefab);
+//                obj.transform.SetParent(myParent.transform);
+//             }
+//         }
+//     }
--- Assets/MyGame/Scripts/Manager1.cs
+++ Assets/MyGame/Scripts/Manager1.cs
@@ -1,60 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-
 public class Manager1 : MonoBehaviour
 {
-    [SerializeField] private TableLayout tableLayout; // Enthält Zeilen, Spalten und Abstände
-    [SerializeField] private GameObject tablePrefab; // Prefab für den Tisch
-    [SerializeField] private GameObject chairPrefab; // Prefab für den Stuhl
-
+    [SerializeField] private TableLayout tableLayout;
+    [SerializeField] private StudentData[] students;
+    [SerializeField] private GameObject tablePrefab;
+    [SerializeField] private GameObject chairPrefab;
     private void Start()
-    {
-        // Validierung vor der Hauptlogik
-        if (tableLayout == null || tablePrefab == null || chairPrefab == null)
-        {
-            Debug.LogError("Manager1 cannot start because required fields are missing.");
-            return;
-        }
-
-        // Tische und Stühle in einem Grid platzieren
+    {   // Tische und Stühle in einem Grid platzieren
         for (int row = 0; row < tableLayout.row; row++)
         {
-            for (int col = 0; col < tableLayout.column; col++)
+            for(int col = 0; col < tableLayout.column; col++)
             {
-                // Berechnung der Tischposition basierend auf den Abstandsangaben
-                Vector3 tablePosition = new Vector3(
-                    col * tableLayout.tableSpacing,
-                    0,
-                    row * tableLayout.tableSpacing
-                );
-
-                // Tisch erstellen und den Manager als Parent setzen mittels transform
-                GameObject table = Instantiate(tablePrefab, tablePosition, Quaternion.identity, transform);
-
+                Vector3 tabelPosition = new Vector3 (col* tableLayout.tableSpacing,0,row* tableLayout.tableSpacing);
+                 // Tisch erstellen und den Manager als Parent setzen mittels transform
+                GameObject table = Instantiate(tablePrefab, tabelPosition, Quaternion.identity, transform);
                 // Stuhlpositionen innerhalb des Tisches finden
-                Transform chairPos1 = table.transform.Find("ChairPos1");
-                Transform chairPos2 = table.transform.Find("ChairPos2");
+                Transform pos1 = table.transform.Find("ChairPos1");
+                Transform pos2 = table.transform.Find("ChairPos2");
+ 
+                if (pos1 != null)
+                { 
+                    Instantiate(chairPrefab, pos1.position, pos1.rotation,table.transform);
+                }
+                if (pos2 != null)
+                {
+                    Instantiate(chairPrefab, pos2.position, pos2.rotation, table.transform);
+                }
 
-                // Stühle erstellen, falls die Positionen existieren
-                if (chairPos1 != null)
-                {
-                    Instantiate(chairPrefab, chairPos1.position, chairPos1.rotation, table.transform);
-                }
-                else
-                {
-                    Debug.LogWarning("ChairPos1 not found in tablePrefab.");
-                }
-
-                if (chairPos2 != null)
-                {
-                    Instantiate(chairPrefab, chairPos2.position, chairPos2.rotation, table.transform);
-                }
-                else
-                {
-                    Debug.LogWarning("ChairPos2 not found in tablePrefab.");
-                }
             }
+
         }
+
     }
+ 
 }
 //wie gebe ich an das alle clone auf dem manager sind
 //Manager parent vom clone machen
